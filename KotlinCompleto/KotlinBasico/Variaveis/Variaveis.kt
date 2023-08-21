9e1296f531ce16089a3c0b01274dd5fd82e703b7
Wesley Martins <wesley.mlopes@castgroup.com>
2023-08-21 08:00:57 -0300

    variaveis e att gitignore

---
+++ KotlinCompleto/KotlinBasico/Variaveis/Variaveis.kt
@@ -0,0 +1,14 @@
+
+
+fun main() {
+    var a: String = "Desenvolvedor Mobile"
+    println(a)
+    
+    val b: Int = 1
+    //val é imutável, assim que atribuida não é possivel muda-la
+    println(b)
+
+    //Variaveis mutaveis ou imutaveis podem ser atribuidas com o tipo ou não, assim o proprio kotlin faz a inferencia do tipo
+    val c = 50.0
+    println(c)
+}
