56d2ff6c884cc64a6e9ba4f98466510672089abd
Wesley Martins <wesley.mlopes@castgroup.com>
2023-08-22 17:32:58 -0300

    estrutura For

---
+++ KotlinCompleto/ControleDeFluxo/For/For.kt
@@ -0,0 +1,10 @@
+
+// Loop: For para percorrer elementos de uma lista
+
+fun main() {
+    val biscoitos = listOf("Chocolate", "Morango", "Baunilha", "Chocolate Branco")
+
+    for (biscoito in biscoitos) {
+        println("Sabores disponiveis: Biscoito de ${biscoito}")
+    }
+}
