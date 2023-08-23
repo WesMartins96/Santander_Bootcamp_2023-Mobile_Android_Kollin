a0c4607a2661ab3a95ad957c15ff645322e7f8cd
Wesley Martins <wesley.mlopes@castgroup.com>
2023-08-23 07:11:33 -0300

    while e do-while

--- KotlinCompleto/ControleDeFluxo/WhileDoWhile/WhileDoWhile.kt
+++ KotlinCompleto/ControleDeFluxo/WhileDoWhile/WhileDoWhile.kt
@@ -1,6 +1,26 @@
+//while(enquanto)
 
-
+// uso do While e Do-while
 
 fun main() {
-    
+    var bolosComidos = 0
+    var bolosAssados = 0
+
+//usando while
+    while(bolosComidos < 5){
+        comerBolo()
+        bolosComidos++
+    }
+
+//usando do-while
+    do{
+        fazerBolo()
+        bolosAssados++
+    } while(bolosAssados < bolosComidos)
 }
+
+fun comerBolo(){
+    println("Comendo o Bolo")
+}
+
+fun fazerBolo() = println("Fazendo o Bolo")
